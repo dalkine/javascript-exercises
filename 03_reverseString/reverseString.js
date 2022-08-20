85711279c62005fc9614c84f5da7c2c19a607615
Adrian Larrea <larrea.adrian704@gmail.com>
2022-08-20 20:51:13 -0300

    Finish 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function (strings) {
+  let array = strings.split(" ");
+  let result = [];
+  for (let index = 0; index < array.length; index++) {
+    let element = array[index];
+    element = element.split("");
+    element.reverse();
+    result.push(element.join(""));
+  }
+  result.reverse();
+  result = result.join(" ");
+  return result;
 };
 
 // Do not edit below this line
