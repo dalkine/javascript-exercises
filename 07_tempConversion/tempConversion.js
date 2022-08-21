f6d242f2d8708a597d52bbf880a37d50ee5b1f44
Adrian Larrea <larrea.adrian704@gmail.com>
2022-08-20 22:44:56 -0300

    Finish 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,22 @@
-const ftoc = function() {
-
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+const ftoc = function (temp) {
+  let celsius = (temp - 32) * (5 / 9);
+  celsius = round(celsius, 1);
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let far = temp * (9 / 5) + 32;
+  far = round(far, 1);
+  return far;
+  //[°C] × 9⁄5 + 32
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
