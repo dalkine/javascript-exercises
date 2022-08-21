ec6cd353199e8588a3ada2d1c70de378cf4fb4e6
Adrian Larrea <larrea.adrian704@gmail.com>
2022-08-20 22:11:21 -0300

    Finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...extract) {
+  let args = extract;
+  let result = array;
 
+  for (let index = 0; index < args.length; index++) {
+    result = result.filter((element) => element !== args[index]);
+  }
+
+  return result;
 };
-
+removeFromArray([1, 2, 3, 4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
