ceac1b68680bb248d1c7304a53778ecf7d2db00e
Adrian Larrea <larrea.adrian704@gmail.com>
2022-08-20 22:30:14 -0300

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (startnum, endnum) {
+  let result = 0;
+  if (startnum > endnum) {
+    let aux = startnum;
+    startnum = endnum;
+    endnum = aux;
+  }
+  if (
+    typeof startnum !== "number" ||
+    typeof endnum !== "number" ||
+    endnum < 0 ||
+    startnum < 0
+  ) {
+    result = "ERROR";
+  } else {
+    for (let index = startnum; index <= endnum; index++) {
+      result += index;
+    }
+  }
 
+  return result;
 };
 
 // Do not edit below this line
